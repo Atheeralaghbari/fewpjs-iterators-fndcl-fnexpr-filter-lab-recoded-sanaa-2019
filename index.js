954c5f1d72d81802242acbad5b657cc78fadf19c
Atheer Alaghbari <atheeralaghbari61@gmail.com>
2019-11-15 17:44:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,13 +4,16 @@ const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
 function findMatching(drivers,param){
    return drivers.filter(e=>e.toLowerCase()===param.toLowerCase())
 }
+console.log(findMatching(drivers,"Bobby"));
+
 function fuzzyMatch(drivers,param){
     return drivers.filter(e=>e.startsWith(param));
 }
+console.log(findMatching(drivers,"S"));
 function matchName(drivers,param){
 return drivers.filter(e=>e.name===param);
 }
 
-console.log(findMatching(drivers,"Bobby"));
-console.log(findMatching(drivers,"S"));
+
+
 // console.log(findMatching(drivers,"Bobby"));
